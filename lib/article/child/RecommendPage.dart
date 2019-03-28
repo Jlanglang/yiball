c0aeda9a88851ebdb438b21c9e4563ffcaf29bf0
Jlanglang <347891332@qq.com>
2019-03-28 18:02:39 +0800

    简书首页

--- lib/article/child/RecommendPage.dart
+++ lib/article/child/RecommendPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 ///推荐
 class RecommendPage extends StatefulWidget {
@@ -7,52 +8,63 @@ class RecommendPage extends StatefulWidget {
 }
 
 class _RecommendPageState extends State<RecommendPage> {
+  Future<String> getData() async {
+    return await Future.delayed(Duration(seconds: 2), () => '请求数据成功');
+  }
+
   Widget buildList(BuildContext context, int index) {
-    return Container(
-        color: Colors.white,
-        padding: EdgeInsets.all(10.0),
-        margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          key: Key(index.toString()),
-          children: <Widget>[
-            new Text(
-              'QWERQWER233323323233332323QWERQWER233323323233332323',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            new Row(
+    return GestureDetector(
+        onTap: () {
+          print("123");
+          getData().then((s) {
+            print(s);
+          });
+        },
+        child: Container(
+            color: Color(0xffffffff),
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                new Expanded(
-                  child: Image(
-                      image: NetworkImage(
-                          'http://pic22.nipic.com/20120714/9622064_105642209176_2.jpg')),
-                  flex: 1,
+                new Text(
+                  'QWERQWER233323323233332323QWERQWER233323323233332323',
+                  style: TextStyle(fontSize: 20.0),
                 ),
-                new Expanded(
-                  child: Image(
-                      image: NetworkImage(
-                          'http://pic22.nipic.com/20120714/9622064_105642209176_2.jpg')),
-                  flex: 1,
+                new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: Image(
+                          image: NetworkImage(
+                              'http://pic22.nipic.com/20120714/9622064_105642209176_2.jpg')),
+                      flex: 1,
+                    ),
+                    new Expanded(
+                      child: Image(
+                          image: NetworkImage(
+                              'http://pic22.nipic.com/20120714/9622064_105642209176_2.jpg')),
+                      flex: 1,
+                    ),
+                    new Expanded(
+                      child: Image(
+                          image: NetworkImage(
+                              'http://pic22.nipic.com/20120714/9622064_105642209176_2.jpg')),
+                      flex: 1,
+                    ),
+                  ],
                 ),
-                new Expanded(
-                  child: Image(
-                      image: NetworkImage(
-                          'http://pic22.nipic.com/20120714/9622064_105642209176_2.jpg')),
-                  flex: 1,
-                ),
+                new Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.details, size: 15.0, color: Colors.redAccent),
+                    Text('1.823',
+                        style:
+                            TextStyle(color: Colors.redAccent, fontSize: 15.0)),
+                    Text(' jlanglang ${123} 阅读 ${123} 评论 ${3} 喜欢'),
+                  ],
+                )
               ],
-            ),
-            new Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.details, size: 15.0, color: Colors.redAccent),
-                Text('1.823',
-                    style: TextStyle(color: Colors.redAccent, fontSize: 15.0)),
-                Text(' jlanglang ${123} 阅读 ${123} 评论 ${3} 喜欢'),
-              ],
-            )
-          ],
-        ));
+            )));
   }
 
   @override
